4662eeb8be604436420e09768c1daf31e0cf4700
Tássio Jordão <tassio72@hotmail.com>
2019-07-10 00:46:19 -0300

    componente de sugestão de mentor

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,19 @@
 <template>
-  <v-toolbar app>
+  <v-toolbar row wrap app>
     <v-toolbar-title class="headline text-uppercase">
       <img src alt /> Logo
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-      <span class="mr-2">Como funciona</span>
-    </v-btn>
-    <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-      <span class="mr-2">Planos</span>
-    </v-btn>
-    <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-      <span class="mr-2">Acessar</span>
-    </v-btn>
+    
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Como funciona</span>
+      </v-btn>
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Planos</span>
+      </v-btn>
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Acessar</span>
+      </v-btn>
   </v-toolbar>
 </template>
 
--- src/components/mentorSuggestion/mentorSuggestion.vue
+++ src/components/mentorSuggestion/mentorSuggestion.vue
@@ -1,16 +1,74 @@
 <template>
-  <h1>Parabéns</h1>
+  <v-container fluid>
+    <!-- card de cadastro -->
+    <v-layout v-show="success === true">
+      <h2>CADASTRO REALIZADO COM SUCESSO!</h2>
+      <p>Fique atento ao seu e-mail. Em até 30 minutos você receberá detalhes da sua mentoria.</p>
+      <v-btn @click="nextSlide">Ok</v-btn>
+    </v-layout>
+    <!-- notificação de sugestão de mentor -->
+    <v-layout v-show="suggestion === true">
+      <img src alt="Fogos de artificio para comemoração" />
+      <h2>Encontramos o mentor ideal para você!</h2>
+      <v-btn @click="searchMentor">VER AGORA!</v-btn>
+    </v-layout>
+    <!-- mentor -->
+    <v-layout v-show="mentorSuggestion === true">
+      <div>
+        <img :src="mentor.photo" :alt="`mentor sugerido: ${mentor.name}`" />
+        <div>
+          <h3>{{ mentor.name}}</h3>
+          <h4>{{ mentor.cargo}}</h4>
+        </div>
+        <div>
+          <h4>Experiências com:</h4>
+          <div>
+            <span v-for="tag in mentor.tags" :key="tag">{{tag}}</span>
+          </div>
+          <p>{{ mentor.description}}</p>
+        </div>
+      </div>
+      <v-btn @click="match">QUERO RESERVAR!</v-btn>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      success: true,
+      suggestion: false,
+      mentorSuggestion: false,
+      mentor: {
+        name: "Tássio Jordão",
+        cargo: "Hacker",
+        photo:
+          "http://varelanoticias.com.br/wp-content/uploads/2017/08/sidney-magal.jpg",
+        tags: ["músico", "garanhão"],
+        description: "Cantor que fez sucesso. Experiência em cantar"
+      }
+    };
   },
   props: {
     title: String,
     likes: Number,
     isPublished: Boolean
+  },
+  methods: {
+    nextSlide() {
+      this.success = false;
+      this.suggestion = true;
+    },
+    searchMentor() {
+      //lógica para comparar os dados recebidos com os mentores cadastrados no banco
+
+      this.suggestion = false;
+      this.mentorSuggestion = true;
+    },
+    match() {
+      console.log("reservado");
+    }
   }
 };
 </script>
