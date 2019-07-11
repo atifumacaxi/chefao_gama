6fcfdfdefdc1485ba3e568aa2bfac376c0b0d3ae
unknown <atifumacaxi@gmail.com>
2019-07-10 22:17:40 -0300

    adding background image from home and initiating css

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,8 @@
   <div id="home">
     <!-- Aqui vamos configurar a tela inicial -->
     <!-- Esse componente só será renderizado em '/' -->
-    <h1>Deslanche em sua carreira</h1>
+    <h1>DESLANCHE EM SUA CARREIRA!</h1>
+    <h2>receba dicas e orientações de Profissionais experientes em sua área</h2>
     <!-- <router-link to="/cadastro" tag="span">teste</router-link> -->
     <v-btn class="btn" flat to="/cadastro">COMEÇAR AGORA</v-btn>
   </div>
@@ -16,4 +17,24 @@ export default {
 
 
 <style scoped>
+#home {
+  flex-direction: column;
+
+  width: 100%;
+  height: auto;
+  background-image: url("imgs/bg-min.png");
+  /* background-size: cover; */
+  background-repeat: no-repeat;
+
+}
+
+h1, h2 {
+  color: white;
+  font-variant-caps: all-petite-caps;
+}
+
+h1 {
+  font-size: 20px;
+}
+  
 </style>
